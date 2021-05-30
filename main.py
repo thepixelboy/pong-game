c1ae21d8ab1b4a4f0ca6eb237a640d9dbe9c85f8
Ruben <34570952+thepixelboy@users.noreply.github.com>
2021-05-30 12:28:30 +0200

    feat: detect when the ball goes ot of bounds

--- main.py
+++ main.py
@@ -39,4 +39,12 @@ while is_game_on:
   if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
     ball.bounce_x()
 
+  # Detect r_paddle misses
+  if ball.xcor() > 380:
+    ball.reset_position()
+  
+  # Detect l_maddle misses
+  if ball.xcor() < -380:
+    ball.reset_position()
+
 screen.exitonclick()
